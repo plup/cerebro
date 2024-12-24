185e412798b34b2b996a105c00f9acc97e38002f
Plup <plup@plup.io>
2024-12-24 15:46:53 +0100

    feat: added more boilerplate code into the demo job

--- tests/job.py
+++ tests/job.py
@@ -2,33 +2,74 @@ import logging
 import argparse
 import sys
 from os import environ
-from thehive4py import TheHiveApi
+from requests import Session
+from requests.exceptions import HTTPError
+from urllib.parse import urljoin
 
-logging.basicConfig(format=None)
+class ThehiveClient(Session):
+    def __init__(self,
+                 base_url = environ['TH_URL'],
+                 key = environ.get('TH_KEY'),
+                 header_user = environ.get('TH_USER'),
+                ):
+        super().__init__()
+        self.base_url = base_url
+        self.headers = {}
+        if key:
+            self.headers['Authorization'] = f'Bearer {key}'
+        if header_user:
+            self.headers['X-TheHive'] = header_user
+
+    def request(self, method, url, *args, **kwargs):
+        joined_url = urljoin(self.base_url, url)
+        return super().request(method, joined_url, *args, **kwargs)
 
 if __name__ == '__main__':
     """Execute the job script with arguments passed from the runner."""
     try:
         # parse arguments
-        parser = argparse.ArgumentParser(description='Run the test job')
+        parser = argparse.ArgumentParser(description='Run the job')
         parser.add_argument('--object-type', required=True)
         parser.add_argument('--object-id', required=True)
         parser.add_argument('--context-type', default=None, choices=['alert', 'case'])
         parser.add_argument('--context-id', default=None)
         args = parser.parse_args()
-        if args.object_type != 'thehive:case_artifact':
-            raise NotImplementedError(f'The object type {args.object_type} is not supported by this job')
 
-        # initialize thehive client
-        hive = TheHiveApi(url=environ['TH_URL'], apikey=environ['TH_KEY'])
+        try:
+            # initialize thehive client
+            thehive = ThehiveClient()
 
-        # do something with thehive api
-        print(hive.observable.get(args.object_id))
+        except KeyError as e:
+            print(f'Missing configuration: {e}')
+            sys.exit(1)
 
-    except KeyError as e:
-        logging.critical(f'Environment missing: {e}')
-        sys.exit(1)
+        try:
+            # do something with thehive api
+            if args.object_type == 'thehive:alert':
+                r = thehive.get(f'/api/v1/alert/{args.object_id}')
+                r.raise_for_status()
+                print('Alert title:', r.json()['title'])
+
+            if args.object_type == 'thehive:case':
+                r = thehive.get(f'/api/v1/case/{args.object_id}')
+                r.raise_for_status()
+                print('Case title:', r.json()['title'])
+
+            if args.object_type == 'thehive:case_artifact':
+                r = thehive.get(f'/api/v1/{args.context_type}/{args.context_id}')
+                r.raise_for_status()
+                context = r.json()
+
+                r = thehive.get(f'/api/v1/observable/{args.object_id}')
+                r.raise_for_status()
+                observable = r.json()
+
+                print(f"Observable {observable['dataType']} from {args.context_type} {context['title']}")
+
+        except HTTPError as e:
+            print(f'Connection error: {e}')
+            sys.exit(1)
 
     except Exception as e:
-        logging.critical(str(e))
+        print(f'Unhandled exception: {e}')
         sys.exit(1)
